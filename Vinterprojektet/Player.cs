9ae4aa452642c85eedf25c752362cba0eba483e7
Maximilian Persson <Maximilian.persson2@elev.ga.ntig.se>
2023-11-20 13:40:38 +0100

    2nd commit

---
+++ Vinterprojektet/Player.cs
@@ -0,0 +1,36 @@
+public class Player
+{
+public string name;
+public string Money;
+public void Cash ()
+{
+Player amount = new Player();
+Money.amount = Console.ReadLine();
+Console.WriteLine("You have ");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- Vinterprojektet/Program.cs
+++ Vinterprojektet/Program.cs
@@ -4,3 +4,12 @@ Console.ReadLine();
 Console.WriteLine("You will be making a stew with your choice of ingredients");
 Console.ReadLine();
 
+Console.WriteLine("The jury will be judging you based on the ingredients you pick");
+Console.ReadLine();
+
+Player Contestant = new Player(); 
+Console.WriteLine("Please enter your name");
+Contestant.name = Console.ReadLine();
+
+Console.WriteLine($"Hi {Contestant.name}");
+Console.ReadLine();
